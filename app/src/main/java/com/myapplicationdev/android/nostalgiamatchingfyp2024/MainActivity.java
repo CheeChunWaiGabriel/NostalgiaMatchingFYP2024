263a2c4b47aee5b8eeec7ab195e4c614879994e0
haixuan <132028953+HuHaixuan@users.noreply.github.com>
2024-04-28 19:01:13 +0800

    added seeting page

--- app/src/main/java/com/myapplicationdev/android/nostalgiamatchingfyp2024/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/nostalgiamatchingfyp2024/MainActivity.java
@@ -1,5 +1,6 @@
 package com.myapplicationdev.android.nostalgiamatchingfyp2024;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Open settings activity
+                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/com/myapplicationdev/android/nostalgiamatchingfyp2024/SettingActivity.java
@@ -0,0 +1,47 @@
+package com.myapplicationdev.android.nostalgiamatchingfyp2024;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SettingActivity extends AppCompatActivity {
+
+    Button btnHomePage;
+    Button btnKYDZweb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_setting);
+
+        btnHomePage = findViewById(R.id.buttonHomePage);
+        btnKYDZweb = findViewById(R.id.buttonKYDZWeb);
+
+        btnHomePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnKYDZweb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent. ACTION_VIEW, Uri.parse("https://kydz.com.sg/"));
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
