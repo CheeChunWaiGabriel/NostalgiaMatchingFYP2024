73bd060784726ff6b2288ae145c0b0d0cc96190d
haixuan <132028953+HuHaixuan@users.noreply.github.com>
2024-05-15 14:37:48 +0800

    easyPage Done

--- app/src/main/java/com/myapplicationdev/android/nostalgiamatchingfyp2024/EasyActivity.java
+++ app/src/main/java/com/myapplicationdev/android/nostalgiamatchingfyp2024/EasyActivity.java
@@ -1,15 +1,12 @@
 package com.myapplicationdev.android.nostalgiamatchingfyp2024;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
 
 public class EasyActivity extends AppCompatActivity {
     Button btnPuzzle1;
@@ -21,7 +18,7 @@ public class EasyActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_puzzle_easy);
 
-        btnPuzzle1 = findViewById(R.id.buttonPuzzle1);
+        btnPuzzle1 = findViewById(R.id.btnFlask);
 
         btnPuzzle1.setOnClickListener(new View.OnClickListener() {
             @Override
