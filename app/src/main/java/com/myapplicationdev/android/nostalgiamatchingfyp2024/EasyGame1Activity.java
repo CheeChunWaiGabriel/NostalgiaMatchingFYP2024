0c8eae2e75bb7a317265caa940686a1f3c573cec
haixuan <132028953+HuHaixuan@users.noreply.github.com>
2024-05-19 20:51:20 +0800

    easy game 1

--- app/src/main/java/com/myapplicationdev/android/nostalgiamatchingfyp2024/EasyGame1Activity.java
+++ app/src/main/java/com/myapplicationdev/android/nostalgiamatchingfyp2024/EasyGame1Activity.java
@@ -1,22 +1,22 @@
 package com.myapplicationdev.android.nostalgiamatchingfyp2024;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.graphics.Color;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class EasyGame1Activity extends AppCompatActivity {
-
     ImageButton imgBtnCorrect1;
     ImageButton imgBtnCorrect2;
     ImageButton imgBtnWrong1;
     ImageButton imgBtnWrong2;
+    Button btnEndGame;
+    GameRun gameRun = new GameRun();
+    Drawable redBG = BGcolor.redBG(20, 10);
+    Drawable greenBG = BGcolor.greenBG(20, 10);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +27,45 @@ public class EasyGame1Activity extends AppCompatActivity {
         imgBtnCorrect1 = findViewById(R.id.imageButtonCorrect1);
         imgBtnCorrect2 = findViewById(R.id.imageButtonCorrect2);
         imgBtnWrong1 = findViewById(R.id.imageButtonWrong1);
-        imgBtnWrong1 = findViewById(R.id.imageButtonWrong1);
-
+        imgBtnWrong2 = findViewById(R.id.imageButtonWrong2);
+        btnEndGame = findViewById(R.id.buttonChooseDiff);
+        
         imgBtnCorrect1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imgBtnCorrect1.setBackgroundColor(Color.GREEN);
+                imgBtnCorrect1.setBackground(greenBG);
+                gameRun.correctOptionFound();
+                gameRun.endGameEasy();
+                if (gameRun.getGameEnded()) {
+                    btnEndGame.setVisibility(View.VISIBLE);
+                }
+                imgBtnCorrect1.setClickable(false);
             }
         });
+        imgBtnCorrect2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgBtnCorrect2.setBackground(greenBG);
+                gameRun.correctOptionFound();
+                gameRun.endGameEasy();
+                if (gameRun.getGameEnded()) {
+                    btnEndGame.setVisibility(View.VISIBLE);
+                }
+                imgBtnCorrect2.setClickable(false);
+            }
+        });
+        imgBtnWrong1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgBtnWrong1.setBackground(redBG);
 
+            }
+        });
+        imgBtnWrong2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgBtnWrong2.setBackground(redBG);
+            }
+        });
     }
 }
